2d2b954b70df64649815bafabc126a32405fd3c8
Unai Zalakain <unai@gisa-elkartea.org>
2017-05-03 22:06:07 +0200

    Add some quick tests for the approval queryset

--- tk/material/tests.py
+++ tk/material/tests.py
@@ -1,3 +1,35 @@
+from datetime import timedelta
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Approval, Activity, Goal
+
+class ApprovalQuerySetTestCase(TestCase):
+    def setUp(self):
+        self.material = Activity.objects.create(
+                title="test",
+                goal=Goal.objects.create(name="test"),
+                duration=timedelta(seconds=30),
+                num_people=10,
+                )
+
+    def test_without_request_is_approved(self):
+        self.assertTrue(Activity.objects.approved().exists())
+
+    def test_accepted_material_is_approved(self):
+        Approval.objects.create(content_object=self.material, approved=True)
+        self.assertTrue(Activity.objects.approved().exists())
+
+    def test_rejected_material_is_not_approved(self):
+        Approval.objects.create(content_object=self.material, approved=False)
+        self.assertFalse(Activity.objects.approved().exists())
+
+    def test_without_is_not_unapproved(self):
+        self.assertFalse(Activity.objects.unapproved().exists())
+
+    def test_accepted_material_is_not_unnaproved(self):
+        Approval.objects.create(content_object=self.material, approved=True)
+        self.assertFalse(Activity.objects.unapproved().exists())
+
+    def test_rejected_material_is_unapproved(self):
+        Approval.objects.create(content_object=self.material, approved=False)
+        self.assertTrue(Activity.objects.unapproved().exists())
