a5e1a5b919389091162e7fed19e042b4d7720733
Unai Zalakain <unai@gisa-elkartea.org>
2017-05-10 13:26:08 +0200

    Load modeltranslation app before admin app

--- tk/settings.py
+++ tk/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Has to come before admin
+    'modeltranslation',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'modeltranslation',
 
     'tk.material',
 ]
