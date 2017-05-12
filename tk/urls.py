8d04bb798648980a2fe29ee408bdcff099bfd2c1
Unai Zalakain <unai@gisa-elkartea.org>
2017-05-12 11:35:39 +0200

    Add a frontpage rendering view

--- tk/urls.py
+++ tk/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
+        url(r'^$', TemplateView.as_view(template_name='base.html'), name='frontpage'),
+
         url(r'^material/', include('tk.material.urls')),
         url(r'^admin/', admin.site.urls),
 ]
