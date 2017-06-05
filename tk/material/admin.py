b997c5c021e264e150a7c6a4cd6435cd524d1598
Unai Zalakain <unai@gisa-elkartea.org>
2017-06-05 17:57:23 +0200

    Fix admin links between material types and approvals

--- tk/material/admin.py
+++ tk/material/admin.py
@@ -1,10 +1,13 @@
-from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.urlresolvers import reverse
+from django.utils.html import format_html
+from django.utils.translation import ugettext_lazy as _
 
 from modeltranslation.admin import TranslationAdmin
 
 from .models import (Subject, Goal, GroupFeature, Location, Language,
-                     Approval, Activity, Reading, Video, Link)
+                     Approval, Material, Activity, Reading, Video, Link)
 
 
 # TODO: Do not delete related
@@ -16,20 +19,44 @@ class ApprovalAdmin(admin.ModelAdmin):
     # TODO: Delete related material action
     # TODO: Approve action or button?
     # TODO: No add view
-    list_filter = ['requested', 'published', 'approved']
-    list_display = ['__str__', 'requested', 'email', 'approved', 'published']
-    fields = [('requested', 'email'), ('published', 'approved'),
-              'material', 'comment']
-    readonly_fields = ['requested', 'published', 'email', 'material']
+    list_filter = ['requested', 'approved']
+    list_display = ['__str__', 'requested', 'email', 'approved']
+    fields = [('requested', 'email'), 'approved', 'material_link', 'comment']
+    readonly_fields = ['requested', 'email', 'material_link']
+
+    def material_link(self, approval_obj):
+        for material_type in ['activity', 'video', 'reading', 'link']:
+            name = 'admin:material_{}_change'.format(material_type)
+            try:
+                obj = getattr(approval_obj.material, material_type)
+            except ObjectDoesNotExist:
+                pass
+            else:
+                return format_html('<a href="{}">{}</a>', reverse(name,
+                    args=[obj.pk]), obj)
+    material_link.short_description = _('Material')
+
+
+class MaterialTypeMixin():
+    def approval_link(self, material_obj):
+        name = 'admin:material_approval_change'
+        return format_html('<a href="{}">{}</a>'.format(
+            reverse(name, args=[material_obj.approval.pk]),
+            material_obj.approval))
+    approval_link.short_description = _('Approval')
 
 
 @admin.register(Activity)
-class ActivityAdmin(TranslationAdmin):
+class ActivityAdmin(TranslationAdmin, MaterialTypeMixin):
     fieldsets = [
+            (_("State"), {
+                'fields': ['approval_link',
+                           # TODO: Creation time
+                           ]}),
             (_("Classification"), {
-                'fields': ['goals', 'subject']}),
-            (_("Conditions"), {
-                'fields': [('min_people', 'max_people'),
+                'fields': ['goals',
+                           'subject',
+                           ('min_people', 'max_people'),
                            'location',
                            'duration',
                            'group_feature']}),
@@ -39,14 +66,19 @@ class ActivityAdmin(TranslationAdmin):
     filter_horizontal = ['goals']
     list_filter = ['min_people', 'max_people', 'location', 'duration',
                    'group_feature', 'goals', 'subject']
-    list_display = ['__str__', 'approval']
+    list_display = ['__str__', 'approval_link']
+    readonly_fields = ['approval_link']
     search_fields = ['title', 'brief', 'notes']
 
 
 @admin.register(Reading)
-class ReadingAdmin(TranslationAdmin):
+class ReadingAdmin(TranslationAdmin, MaterialTypeMixin):
     fieldsets = [
-            (_("Details"), {
+            (_("State"), {
+                'fields': ['approval_link',
+                           # TODO: Creation time
+                           ]}),
+            (_("Classification"), {
                 'fields': ['pages',
                            'year',
                            'language',
@@ -54,15 +86,20 @@ class ReadingAdmin(TranslationAdmin):
             (_("Content"), {
                 'fields': ['author', 'title', 'url', 'brief', 'attachment']})
             ]
+    readonly_fields = ['approval_link']
     list_filter = ['pages', 'year', 'language', 'subject']
-    list_display = ['__str__', 'approval']
+    list_display = ['__str__', 'approval_link']
     search_fields = ['title', 'brief']
 
 
 @admin.register(Video)
-class VideoAdmin(TranslationAdmin):
+class VideoAdmin(TranslationAdmin, MaterialTypeMixin):
     fieldsets = [
-            (_("Details"), {
+            (_("State"), {
+                'fields': ['approval_link',
+                           # TODO: Creation time
+                           ]}),
+            (_("Classification"), {
                 'fields': ['duration',
                            'year',
                            'audios',
@@ -71,20 +108,26 @@ class VideoAdmin(TranslationAdmin):
             (_("Content"), {
                 'fields': ['author', 'title', 'url', 'brief', 'attachment']})
             ]
+    readonly_fields = ['approval_link']
     filter_horizontal = ['audios', 'subtitles']
     list_filter = ['duration', 'year', 'audios', 'subtitles', 'subject']
-    list_display = ['__str__', 'approval']
+    list_display = ['__str__', 'approval_link']
     search_fields = ['title', 'brief']
 
 
 @admin.register(Link)
-class LinkAdmin(TranslationAdmin):
+class LinkAdmin(TranslationAdmin, MaterialTypeMixin):
     fieldsets = [
-            (_("Details"), {
+            (_("State"), {
+                'fields': ['approval_link',
+                           # TODO: Creation time
+                           ]}),
+            (_("Classification"), {
                 'fields': ['subject']}),
             (_("Content"), {
                 'fields': ['author', 'title', 'url', 'brief']})
             ]
+    readonly_fields = ['approval_link']
     list_filter = ['subject']
-    list_display = ['__str__', 'approval']
+    list_display = ['__str__', 'approval_link']
     search_fields = ['title', 'brief']
