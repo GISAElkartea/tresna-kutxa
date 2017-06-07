8109ecbe64c729abe0f231b96bcf7b537ebc0895
Unai Zalakain <unai@gisa-elkartea.org>
2017-06-07 12:53:43 +0200

    Configure development media serving

--- tk/urls.py
+++ tk/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
@@ -18,3 +19,4 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
