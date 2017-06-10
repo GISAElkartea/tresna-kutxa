d4e79aa3eff70bbe3020163b4df204673df76ee7
Unai Zalakain <unai@gisa-elkartea.org>
2017-06-10 15:51:36 +0200

    Avoid django-debug-toolbar fetching jquery from Evil Google

--- tk/settings.py
+++ tk/settings.py
@@ -124,3 +124,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '.media')
 
 INTERNAL_IPS = ['127.0.0.1']
+DEBUG_TOOLBAR_CONFIG = {
+        # TODO
+        'JQUERY_URL': '/static/js/jquery.js',
+}
