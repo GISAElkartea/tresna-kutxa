ae4233b53aca2ac76b70c09d0ee67dca6ebf21a9
Uma Zalakain <uma@gisa-elkartea.org>
2018-12-28 14:55:38 +0100

    No need to handle localized field labels ourselfs, upstream does it

--- tk/material/widgets.py
+++ tk/material/widgets.py
@@ -8,12 +8,6 @@ class LocalizedMarkdownxWidget(LocalizedFieldWidget):
     template_name = 'localized_fields/multiwidget.html'
     widget = MarkdownxWidget
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for ((lc, ln), w) in zip(settings.LANGUAGES, self.widgets):
-            w.attrs['lang_code'] = lc
-            w.attrs['lang_name'] = ln
-
 
 class AdminLocalizedMarkdownxWidget(AdminLocalizedFieldWidget):
     widget = AdminMarkdownxWidget
