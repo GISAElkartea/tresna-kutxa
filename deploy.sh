1790e65bba3d8c5d0d551a7a0c04bc7c034a152a
Uma Zalakain <uma@gisa-elkartea.org>
2019-01-04 11:59:43 +0100

    Compile CSS on deployment

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ ssh -T www@biltokia.joxemizumalabe.eus <<'END'
 cd tresna-kutxa
 git pull origin master
 sudo nixos-rebuild switch
-for command in 'collectstatic --noinput' migrate compilemessages; do
+for command in compilescss 'collectstatic --noinput' migrate compilemessages; do
      nix-shell nix --run "python manage.py ${command} --settings=tk.production"
 done
 sudo ../restart-uwsgi.sh
