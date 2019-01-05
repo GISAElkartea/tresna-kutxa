2776aa800a4880f31d109c7e11d6fc72874c6d31
Uma Zalakain <uma@gisa-elkartea.org>
2019-01-05 21:48:16 +0000

    Perform correct overlap lookups for language arrays (fixed #25)

--- tk/material/filtersets.py
+++ tk/material/filtersets.py
@@ -1,7 +1,7 @@
 from django.contrib.postgres.fields import IntegerRangeField
 
 from django_filters import FilterSet
-from django_filters.filters import ChoiceFilter, NumericRangeFilter
+from django_filters.filters import MultipleChoiceFilter, NumericRangeFilter
 
 from .fields import get_languages
 from .models import Material, Activity, Reading, Video, Link, COMMON_LANGUAGES
@@ -23,6 +23,22 @@ class IncludeNullNumericRangeFilter(IncludeNullMixin, NumericRangeFilter):
     pass
 
 
+class ArrayChoiceMixin:
+    def filter(self, qs, value):
+        if not value:
+            # Even though not a noop, no point filtering if empty.
+            return qs
+        if self.is_noop(qs, value):
+            return qs
+        lookup = '%s__%s' % (self.field_name, self.lookup_expr)
+        qs = self.get_method(qs)(**{lookup: value})
+        return qs.distinct() if self.distinct else qs
+
+
+class ArrayMultipleChoiceFilter(ArrayChoiceMixin, MultipleChoiceFilter):
+    pass
+
+
 class ActivityFilterSet(FilterSet):
     num_people = NumericRangeFilter(
             widget=RangeWidget(attrs={'min': 0, 'max': 40}),
@@ -38,7 +54,13 @@ class ActivityFilterSet(FilterSet):
 
 
 class ReadingFilterSet(FilterSet):
-    languages = ChoiceFilter(choices=get_languages(limit_to=COMMON_LANGUAGES))
+    languages = ArrayMultipleChoiceFilter(
+            choices=get_languages(limit_to=COMMON_LANGUAGES),
+            lookup_expr='overlap')
+
+    pages = IncludeNullNumericRangeFilter(
+            widget=RangeWidget(attrs={'min': 0, 'max': 3000}),
+            lookup_expr='range')
 
     class Meta:
         model = Reading
@@ -46,8 +68,17 @@ class ReadingFilterSet(FilterSet):
 
 
 class VideoFilterSet(FilterSet):
-    audios = ChoiceFilter(choices=get_languages(prioritize=COMMON_LANGUAGES))
-    subtitles = ChoiceFilter(choices=get_languages(limit_to=COMMON_LANGUAGES))
+    audios = ArrayMultipleChoiceFilter(
+            choices=get_languages(prioritize=COMMON_LANGUAGES),
+            lookup_expr='overlap')
+
+    subtitles = ArrayMultipleChoiceFilter(
+            choices=get_languages(limit_to=COMMON_LANGUAGES),
+            lookup_expr='overlap')
+
+    duration = IncludeNullNumericRangeFilter(
+            widget=RangeWidget(attrs={'min': 0, 'max': 360}),
+            lookup_expr='range')
 
     class Meta:
         model = Video
