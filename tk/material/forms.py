5c17512ab8c12485e1f487ff20d012509b8ef72f
Uma Zalakain <uma@gisa-elkartea.org>
2019-12-30 00:06:39 +0000

    Restrict the fields that can be set via submission

--- tk/material/forms.py
+++ tk/material/forms.py
@@ -9,6 +9,8 @@ from .widgets import LocalizedMarkdownxWidget, RangeWidget
 MATERIAL_WIDGETS = {'goal': LocalizedMarkdownxWidget,
                     'brief': LocalizedMarkdownxWidget,}
 
+MATERIAL_FIELDS = ['title', 'subjects', 'goal', 'brief', 'author']
+
 class MaterialForm(ModelForm):
     error_css_class = 'error'
     required_css_class = 'required'
@@ -20,31 +22,33 @@ class MaterialForm(ModelForm):
         super().save()
         Approval(email=self.cleaned_data['email'], material=self.instance).save()
         return self.instance
-        
+
 
 class ActivityForm(MaterialForm):
     class Meta:
         model = Activity
-        fields = '__all__'
+        fields = MATERIAL_FIELDS + [
+            'location', 'duration', 'num_people', 'group_feature',
+            'notes', 'attachment', 'url']
         widgets = dict(num_people=RangeWidget(), **MATERIAL_WIDGETS)
 
 
 class VideoForm(MaterialForm):
     class Meta:
         model = Video
-        fields = '__all__'
+        fields = MATERIAL_FIELDS + ['duration', 'year', 'audios', 'subtitles', 'url']
         widgets = MATERIAL_WIDGETS
 
 
 class ReadingForm(MaterialForm):
     class Meta:
         model = Reading
-        fields = '__all__'
+        fields = MATERIAL_FIELDS + ['pages', 'year', 'languages', 'attachment', 'url']
         widgets = MATERIAL_WIDGETS
 
 
 class LinkForm(MaterialForm):
     class Meta:
         model = Link
-        fields = '__all__'
+        fields = MATERIAL_FIELDS + ['url']
         widgets = MATERIAL_WIDGETS
