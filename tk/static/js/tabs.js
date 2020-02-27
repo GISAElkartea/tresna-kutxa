936f280c1103fe9a9d0128b81f00605a2ff41ee0
Uma Zalakain <uma@gisa-elkartea.org>
2020-02-27 18:09:00 +0000

    Solve the tab issues (fixes #41)

--- tk/static/js/tabs.js
+++ tk/static/js/tabs.js
@@ -1,29 +1,25 @@
-function openTab(evt, container, tabName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+function openTab(evt, links, tabs, tab) {
+    links = document.querySelectorAll(links);
+    tabs = document.querySelectorAll(tabs);
+    tab = document.querySelector(tab);
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = container.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+    var i;
+    for (i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = "none";
     }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = container.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    for (i = 0; i < links.length; i++) {
+        links[i].className = links[i].className.replace(" active", "");
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
+    tab.style.display = "block";
     if (evt) {
         evt.currentTarget.className += " active";
     }
 }
 
-function restoreOpenTab(container) {
+function restoreOpenTab(links, tabs) {
     if (window.location.hash) {
-        const tab = decodeURI(window.location.hash.substring(1));
-        openTab(null, container, tab);
+        openTab(null, links, tabs, decodeURI(window.location.hash));
     }
 }
